d9a467b17253f4b2464b5cf927d6940f2f7c14bc
TsubasaK111 <Tsubasa.K111@gmail.com>
2018-07-29 12:17:43 +0900

    refactor: /index.js is a Server class now

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // DEPENDENCIES
 
 const fs = require("fs");
@@ -9,58 +10,69 @@ const db = require("./db")(config);
 // Routes. Initialized 'db' dependencies are explicitly injected.
 const apiRouter = require("./routes/api")(db);
 // logs your requests
-const morgan = require("morgan"); 
+const morgan = require("morgan");
 //enables express to parse JSON
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 
 const express = require("express");
-const app = express();
 
-
-// SERVER SETUP
-// Middleware 'pipeline' setup.
-// https://www.safaribooksonline.com/blog/2014/03/10/express-js-middleware-demystified/
-
-// 1. log every request to stdout AND access.log
-app.use(morgan('dev'));
-let logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
-let logger = morgan('combined', {stream: logStream})
-app.use(logger)
-
-// 2. Set the headers for incoming requests
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,PUT,POST,DELETE,OPTIONS,PATCH"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, authorization"
-  );
-  next();
-});
-
-// 3. Parse request bodies as json
-app.use(bodyParser.json({ type: "application/json", limit: "5mb" }));
-
-// 4. If the requests begin with '/api', hand them off to the API router
-app.use("/api", apiRouter);
-app.use(express.static(`${__dirname}/public`)); // otherwise load the client app
-
-// 5. Catch unhandled errors thrown by any of the previous middleware steps
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  if (err.stack) {
-    if (err.stack.match("node_modules/body-parser")) {
-      return res.status(400).send("Invalid JSON");
-    }
+class Server {
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.start(config.express.port);
   }
-  
-  return res.status(500).send("Internal Error.");
-});
 
-// START SERVER
-app.listen(config.express.port, () => {
-  console.info(`Server up and listening on port ${config.express.port}`);
-});
+  start(port) {
+    this.app.listen(port, () => {
+      console.info(`Server up and listening on port ${port}`);
+    });
+
+  }
+
+  configure() {
+    // Middleware 'pipeline' setup.
+    // https://www.safaribooksonline.com/blog/2014/03/10/express-js-middleware-demystified/
+
+    // 1. log every request to stdout AND access.log
+    this.app.use(morgan('dev'));
+    let logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+    this.logger = morgan('combined', { stream: logStream })
+    this.app.use(this.logger)
+
+    // 2. Set the headers for incoming requests
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET,PUT,POST,DELETE,OPTIONS,PATCH"
+      );
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, authorization"
+      );
+      next();
+    });
+
+    // 3. Parse request bodies as json
+    this.app.use(bodyParser.json({ type: "application/json", limit: "5mb" }));
+
+    // 4. If the requests begin with '/api', hand them off to API router
+    this.app.use("/api", apiRouter);
+    this.app.use(express.static(`${__dirname}/public`)); // otherwise serve client app
+
+    // 5. Catch unhandled errors thrown by any of the previous middleware steps
+    // eslint-disable-next-line no-unused-vars
+    this.app.use((err, req, res, next) => {
+      if (err.stack) {
+        if (err.stack.match("node_modules/body-parser")) {
+          return res.status(400).send("Invalid JSON");
+        }
+      }
+      return res.status(500).send("Internal Error.");
+    });
+
+  }
+}
+
+new Server();
